e3ef1cc83043379e7c1b89f13b5e8d03b0f6cf43
sansoris <sansoris@hotmail.com>
2021-09-08 11:07:31 -0500

    Ciclo3

---
+++ Ejercicio1/init.js
@@ -0,0 +1,32 @@
+
+    // se encuentra en javascrip, alert es ventana emergente y un input con prompt
+    alert("Esta es una ventana emergente en la página web- Hola como estas");
+    
+    // para ingresar información por consola
+    let nombre2;
+    nombre2 = prompt("Digite su nombre completo");
+    document.write(nombre2);
+    document.write('<br>');
+    let cadena  = 'Carlos'; 
+    let edad = 20;
+    let altura = 1.75;
+    let casado = false;
+    document.write(cadena);
+    document.write('<br>');
+    document.write(edad);
+    document.write('<br>');
+    document.write(altura);
+    document.write('<br>');
+    document.write(casado);
+    document.write('<br>');
+
+    let num1= prompt("Digite un numero1: ")
+    let num2= prompt("Digite un numero2: ")
+    let suma = parseInt(num1) + parseInt(num2);
+    let producto = parseInt(num1) * parseInt(num2);
+    document.write('La suma es: ');
+    document.write(suma);
+    document.write('<br>');
+    document.write('El producto es: ');
+    document.write(producto);
+    document.write('<br>');
