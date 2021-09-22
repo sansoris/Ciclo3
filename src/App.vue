826f6cc53c6f3d88f439348cb65a8610179d18e8
sansoris <sansoris@hotmail.com>
2021-09-22 11:05:08 -0500

    iniciovuejs

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,51 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <!-- Titulo 1 del componenente de app -->
+    <h1>{{tituloApp}}</h1>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+      <div v-if= "mostrarNombre" >
+        <h2>{{usuario.nombre}} </h2>
+      </div>
+      <div v-else>
+        <h2> No se puede mostrar el nombre </h2>
+      </div>
+      <h2>{{usuario.apellido}} </h2>
+      <h2>{{usuario.edad}} </h2>
+    <prueba/>
+
+
   </div>
+ 
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import prueba from './components/Prueba.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  // name: 'App',
+  // components: {
+  //   HelloWorld
+  // }
+  // también se pueden trabajar los marcadores y directivas d
+  // Se pueden tener data, como función que retorna lo que se desee entre llaves
+  
+  data(){
+    return{
+        tituloApp:" Este es el título 1 del componente App.vue",
+        usuario:{
+            nombre: "María",
+            apellido: "Rodríguez",
+            edad: 45
+        }, 
+        mostrarNombre: false
+    }
+  },
+  components:{
+    prueba
   }
+
 }
 </script>
 
---
+++ src/components/Prueba.vue
@@ -0,0 +1,48 @@
+<template>
+    <div id= "app2">
+        <h1>{{titulo2}} </h1>
+        <!-- "El ejemplo de operacion" -->
+        <h1>{{2+2}}</h1>
+        <!-- Mostrando el array -->
+        <h1>{{frutas[1]}} </h1>
+        <!-- mostrando una lista se requiere el nombre de la llave (key) -->
+        <div>
+            <ul>
+                <li v-for="fruta in frutas" v-bind:key="fruta">
+                       {{fruta}} 
+                </li>
+            </ul>
+        </div>
+        <div>
+            <button type="button" v-on:click="saludar" >
+                CLICK AQUÍ
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data(){
+        return{
+            titulo2:"Este es el título 2 de componente prueba.vue",
+            // Ejemplo de array
+            frutas:[
+                "Manzanas", 
+                "Pera",
+                "Naranja",
+                "Uva"
+            ]
+        }
+    },
+    methods:{
+        saludar(){
+            alert("Hola cómo estas?");
+        }
+    }
+}
+</script>
+<style>
+
+</style>
